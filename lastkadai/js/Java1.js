cd0c7958a58c66f9931755dc9d205bec5a0d464f
himenosoka <68785731+himenosoka@users.noreply.github.com>
2020-07-26 01:44:13 +0900

    Add files via upload

---
+++ lastkadai/js/Java1.js
@@ -0,0 +1,33 @@
+alert("北海道の情報");
+function quize(kotae){
+    var message="";
+    if(kotae==1){
+        message="残念…答えは、「冷え込む」です‼";
+    }
+    else if(kotae==2){
+        message="正解‼";
+    }
+    else if(kotae==3){
+        message="残念…答えは、「冷え込む」です‼";
+    }
+    else if(kotae==4){
+        message="残念…答えは、「絆創膏」です‼";
+    }
+    else if(kotae==5){
+        message="残念…答えは、「絆創膏」です‼";
+    }
+    else if(kotae==6){
+        message="正解‼";
+    }
+    else if(kotae==7){
+        message="正解‼";
+    }
+    else if(kotae==8){
+        message="残念…答えは、「交換する」です‼";
+    }
+    else if(kotae==9){
+        message="残念…答えは、「交換する」です‼";
+    }
+
+    alert(message);
+}
